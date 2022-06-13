f2c27cabfa4c6219df282af53662d3465bf53a82
G <g@Gs-MacBook-Pro.local>
2022-06-12 22:47:21 -1000

    wip use arb script as jupiter-api, add tweet support for arb trades

---
+++ src/monitoring-service/jupiter-api.ts
@@ -0,0 +1,348 @@
+import * as anchor from '@project-serum/anchor';
+import {
+  AccountMeta,
+  ParsedMessageAccount,
+  Connection,
+  PublicKey,
+  PartiallyDecodedInstruction,
+} from '@solana/web3.js';
+import { IDL } from '../../idl/jupiter';
+import { BorshCoder } from '@project-serum/anchor';
+import type { ParsedAccountData } from '@solana/web3.js';
+import type { Instruction } from '@project-serum/anchor';
+import { InstructionDisplay } from '@project-serum/anchor/dist/cjs/coder/borsh/instruction';
+import { TokenInfo, TokenListProvider } from '@solana/spl-token-registry';
+import { token } from '@project-serum/anchor/dist/cjs/utils';
+
+// TODO add any important arbitrage data required for tweet
+export interface ArbTradeData {
+  tx: anchor.web3.ParsedTransactionWithMeta;
+  source: PublicKey;
+  destination: PublicKey;
+  inAmount: string;
+  minimumOutAmount: string;
+  tokenProgram: PublicKey;
+  tokenMint: ParsedAccountData;
+  tokenData: TokenInfo;
+}
+
+const accountNamesMapping: any = {
+  tokenSwap: {
+    source: sentenceCase('source'),
+    destination: sentenceCase('destination'),
+    tokenProgram: sentenceCase('tokenProgram'),
+  },
+  mercurialExchange: {
+    source: sentenceCase('sourceTokenAccount'),
+    destination: sentenceCase('destinationTokenAccount'),
+    tokenProgram: sentenceCase('tokenProgram'),
+  },
+  serumSwap: {
+    source: sentenceCase('orderPayerTokenAccount'),
+    coin: sentenceCase('coinWallet'),
+    pc: sentenceCase('pcWallet'),
+    tokenProgram: sentenceCase('tokenProgram'),
+  },
+  stepTokenSwap: {
+    source: sentenceCase('source'),
+    destination: sentenceCase('destination'),
+    tokenProgram: sentenceCase('tokenProgram'),
+  },
+  saberExchange: {
+    source: sentenceCase('inputUserAccount'),
+    destination: sentenceCase('outputUserAccount'),
+    tokenProgram: sentenceCase('tokenProgram'),
+  },
+  cropperTokenSwap: {
+    source: sentenceCase('source'),
+    destination: sentenceCase('destination'),
+    tokenProgram: sentenceCase('tokenProgram'),
+  },
+  raydiumSwap: {
+    source: sentenceCase('userSourceTokenAccount'),
+    destination: sentenceCase('userDestinationTokenAccount'),
+    tokenProgram: sentenceCase('tokenProgram'),
+  },
+  raydiumSwapV2: {
+    source: sentenceCase('userSourceTokenAccount'),
+    destination: sentenceCase('userDestinationTokenAccount'),
+    tokenProgram: sentenceCase('tokenProgram'),
+  },
+  aldrinSwap: {
+    base: sentenceCase('userBaseTokenAccount'),
+    quote: sentenceCase('userQuoteTokenAccount'),
+    tokenProgram: sentenceCase('tokenProgram'),
+  },
+  aldrinV2Swap: {
+    base: sentenceCase('userBaseTokenAccount'),
+    quote: sentenceCase('userQuoteTokenAccount'),
+    tokenProgram: sentenceCase('tokenProgram'),
+  },
+  cremaTokenSwap: {
+    source: sentenceCase('userSourceTokenAccount'),
+    destination: sentenceCase('userDestinationTokenAccount'),
+    tokenProgram: sentenceCase('tokenProgram'),
+  },
+  senchaExchange: {
+    source: sentenceCase('inputUserAccount'),
+    destination: sentenceCase('outputUserAccount'),
+    tokenProgram: sentenceCase('tokenProgram'),
+  },
+};
+
+// This methods will extract the user sender and receiver token accounts for each instruction
+function getSenderAndReceiverTokenAccounts(
+  accountKeys: ParsedMessageAccount[],
+  instruction: PartiallyDecodedInstruction,
+) {
+  if (!instruction || !instruction.accounts) {
+    return null;
+  }
+
+  const coder = new BorshCoder(IDL);
+  const accountMetas: AccountMeta[] = instruction.accounts.map((pubkey) => {
+    const accountKey = accountKeys.find((ak) => {
+      return ak.pubkey.equals(pubkey);
+    });
+
+    return {
+      pubkey,
+      isSigner: accountKey ? accountKey.signer : false,
+      isWritable: accountKey ? accountKey.writable : false,
+    };
+  });
+
+  const ix = coder.instruction.decode(instruction.data, 'base58');
+
+  if (ix == null) {
+    return;
+  }
+
+  if (!Object.keys(accountNamesMapping).includes(ix.name)) {
+    return null;
+  }
+
+  const format = coder.instruction.format(ix, accountMetas);
+
+  if (format == null) {
+    return;
+  }
+
+  return extractSenderAndReceiverTokenAccounts(ix, format);
+}
+
+function sentenceCase(field: string): string {
+  const result = field.replace(/([A-Z])/g, ' $1');
+  return result.charAt(0).toUpperCase() + result.slice(1);
+}
+
+function extractSenderAndReceiverTokenAccounts(
+  ix: Instruction,
+  format: InstructionDisplay,
+) {
+  const accountMapping = accountNamesMapping[ix.name];
+  if (!accountMapping) {
+    return null;
+  }
+
+  let source: PublicKey | undefined;
+  let destination: PublicKey | undefined;
+  let tokenProgram: PublicKey | undefined;
+  let inAmount: string | undefined;
+  let minimumOutAmount: string | undefined;
+
+  const ixData: any = ix.data;
+  // console.log("inamount: ", format.args)
+  // console.log("format: ", format);
+
+  if (!format) {
+    return null;
+  }
+
+  // Serum destination token account depends on the source token account
+  if (ix.name === 'serumSwap') {
+    source = format.accounts.find(
+      ({ name }) => name === accountMapping['source'],
+    )?.pubkey;
+    let coin = format.accounts.find(
+      ({ name }) => name === accountMapping['coin'],
+    )?.pubkey;
+    let pc = format.accounts.find(
+      ({ name }) => name === accountMapping['pc'],
+    )?.pubkey;
+    tokenProgram = format.accounts.find(
+      ({ name }) => name === accountMapping['tokenProgram'],
+    )?.pubkey;
+
+    if (coin != null && source != null) {
+      destination = coin.equals(source) ? pc : coin;
+    }
+
+    inAmount = format.args.find(({ name }) => name === 'inAmount')?.data;
+    minimumOutAmount = format.args.find(
+      ({ name }) => name === 'minimumOutAmount',
+    )?.data;
+  } else if (ix.name === 'aldrinV2Swap' || ix.name == 'aldrinSwap') {
+    let sourceKey = (ix.data as any).side.bid ? 'quote' : 'base';
+    let destinationKey = (ix.data as any).side.bid ? 'base' : 'quote';
+
+    source = format.accounts.find(
+      ({ name }) => name === accountMapping[sourceKey],
+    )?.pubkey;
+    destination = format.accounts.find(
+      ({ name }) => name === accountMapping[destinationKey],
+    )?.pubkey;
+
+    tokenProgram = format.accounts.find(
+      ({ name }) => name === accountMapping['tokenProgram'],
+    )?.pubkey;
+
+    inAmount = format.args.find(({ name }) => name === 'inAmount')?.data;
+    minimumOutAmount = format.args.find(
+      ({ name }) => name === 'minimumOutAmount',
+    )?.data;
+  } else {
+    source = format.accounts.find(
+      ({ name }) => name === accountMapping['source'],
+    )?.pubkey;
+    destination = format.accounts.find(
+      ({ name }) => name === accountMapping['destination'],
+    )?.pubkey;
+
+    tokenProgram = format.accounts.find(
+      ({ name }) => name === accountMapping['tokenProgram'],
+    )?.pubkey;
+
+    inAmount = format.args.find(({ name }) => name === 'inAmount')?.data;
+    minimumOutAmount = format.args.find(
+      ({ name }) => name === 'minimumOutAmount',
+    )?.data;
+  }
+
+  if (!source || !destination || !tokenProgram) {
+    return null;
+  }
+
+  return {
+    source,
+    destination,
+    inAmount,
+    minimumOutAmount,
+    tokenProgram,
+  };
+}
+
+export async function findJupArbTrades(): Promise<ArbTradeData[]> {
+  let arbTrades: ArbTradeData[] = [];
+  const connection = new Connection(process.env.RPC_URL!);
+  const jupiterV2ProgramId = new PublicKey(
+    'JUP2jxvXaqu7NQY1GmNF4m1vodw12LVXYxbFL2uJvfo',
+  );
+
+  const signatures = await connection.getConfirmedSignaturesForAddress2(
+    jupiterV2ProgramId,
+  );
+
+  console.log('signatures: ', signatures.length);
+
+  const txs = await Promise.all(
+    signatures.map(
+      async (signature) =>
+        await connection.getParsedConfirmedTransaction(signature.signature),
+    ),
+  );
+
+  const tokenListProvider = new TokenListProvider();
+
+  const allTokens = await tokenListProvider.resolve();
+
+  const tokensList = allTokens.filterByClusterSlug('mainnet-beta').getList();
+
+  console.log('txs: ', txs[0]?.transaction.message);
+
+  // const transactions = await connection.getTransaction(signatures[0].signature);
+
+  console.log('done fetching transactions we have ', txs.length);
+
+  for (let i = 0; i < txs.length; i++) {
+    // const signature = signatures[i];
+
+    const tx = txs[i];
+
+    if (tx != null) {
+      const ix = tx.transaction.message
+        .instructions[2] as PartiallyDecodedInstruction;
+
+      if (ix == null) {
+        continue;
+      }
+
+      const result = getSenderAndReceiverTokenAccounts(
+        tx.transaction.message.accountKeys,
+        ix,
+      );
+
+      // Found an arb trade
+      if (
+        result &&
+        result.inAmount &&
+        result.minimumOutAmount &&
+        result.inAmount != 'null' &&
+        result.source.toBase58() === result.destination.toBase58() &&
+        parseInt(result.inAmount) < parseInt(result.minimumOutAmount)
+      ) {
+        console.log('source', result.source.toBase58());
+        console.log('destination', result.destination.toBase58());
+        console.log('inAmount', result.inAmount);
+        console.log('minimumOutAmount', result.minimumOutAmount);
+        console.log('tokenProgram', result.tokenProgram.toBase58());
+
+        const sourceParsedAccountInfo = await connection.getParsedAccountInfo(
+          result.source,
+        );
+        const destinationParsedAccountInfo =
+          await connection.getParsedAccountInfo(result.destination);
+
+        if (
+          (sourceParsedAccountInfo.value?.data as ParsedAccountData) &&
+          (destinationParsedAccountInfo.value?.data as ParsedAccountData)
+        ) {
+          console.log(
+            'sourceParsedAccountInfo.parsed: ',
+            (sourceParsedAccountInfo.value?.data as ParsedAccountData).parsed,
+          );
+          console.log(
+            'destinationParsedAccountInfo.parsed: ',
+            (destinationParsedAccountInfo.value?.data as ParsedAccountData)
+              .parsed,
+          );
+
+          const tokenMint = (
+            destinationParsedAccountInfo.value?.data as ParsedAccountData
+          ).parsed.info.mint;
+
+          const tokenData = tokensList.find(
+            ({ address }) => address === tokenMint,
+          );
+
+          console.log('tokenData', tokenData);
+
+          // TODO verify all necessary arbitrage data returned
+          arbTrades.push({
+            tx: tx,
+            source: result.source,
+            destination: result.destination,
+            inAmount: result.inAmount,
+            minimumOutAmount: result.minimumOutAmount,
+            tokenProgram: result.tokenProgram,
+            tokenMint: tokenMint,
+            tokenData: tokenData,
+          } as ArbTradeData);
+        }
+        // console.log("sourceParsedAccountInfo: ", sourceParsedAccountInfo);
+      }
+    }
+  }
+  console.log("Found Jupiter arbitrage trades: ", arbTrades);
+  return arbTrades;
+}
--- src/monitoring-service/monitoring.service.ts
+++ src/monitoring-service/monitoring.service.ts
@@ -7,19 +7,10 @@ import {
 } from '@nestjs/common';
 import { DialectConnection } from './dialect-connection';
 import {
-  getAllProposals,
-  getAllTokenOwnerRecords,
-  getRealms,
-  getTokenOwnerRecord,
-  ProgramAccount,
-  Proposal,
-  Realm,
-} from '@solana/spl-governance';
-import {
   TwitterNotification,
   TwitterNotificationsSink,
 } from './twitter-notifications-sink';
-
+import { findJupArbTrades, ArbTradeData } from './jupiter-api';
 import {
   Monitors,
   NotificationSink,
@@ -28,63 +19,20 @@ import {
   SourceData,
 } from '@dialectlabs/monitor';
 import { Duration } from 'luxon';
+import { TokenInfo } from '@solana/spl-token-registry';
 
-const mainnetPK = new PublicKey('GovER5Lthms3bLBqWub97yVrMmEogzX7xNjdXpPPCVZw');
-
-const connection = new Connection(
-  process.env.REALMS_PRC_URL ?? process.env.RPC_URL!,
-);
-
-interface RealmData {
-  realm: ProgramAccount<Realm>;
-  proposals: ProgramAccount<Proposal>[];
-  realmMembersSubscribedToNotifications: Record<string, PublicKey>;
+interface JupiterArbitrageTrades {
+  arbTrades: ArbTradeData[];
 }
-
-type TokenOwnerRecordToGoverningTokenOwnerType = {
-  [key: string]: string;
-};
-
-/*
-Realms use case:
-When a proposal is added to a realm -
-1. send a tweet out
-
----
-
-* global data fetch
-1. Fetch all realms
-2. Fetch all proposals
-
-* filter or detect diff
-3. Look for diffs in the proposals array
-4. When finding a proposal added or removed
-5. Send out tweet for new proposal
-*/
 
 @Injectable()
 export class MonitoringService implements OnModuleInit, OnModuleDestroy {
-  private readonly notificationSink: NotificationSink<TwitterNotification> =
+  private readonly twitterNotificationSink: NotificationSink<TwitterNotification> =
     new TwitterNotificationsSink();
 
   private readonly logger = new Logger(MonitoringService.name);
-  private tokenOwnerRecordToGoverningTokenOwner: TokenOwnerRecordToGoverningTokenOwnerType =
-    {};
 
   constructor(private readonly dialectConnection: DialectConnection) {}
-
-  private static async getProposals(realm: ProgramAccount<Realm>) {
-    const proposals = (
-      await getAllProposals(connection, mainnetPK, realm.pubkey)
-    ).flat();
-    if (process.env.TEST_MODE) {
-      return proposals.slice(
-        0,
-        Math.round(Math.random() * Math.max(0, proposals.length - 3)),
-      );
-    }
-    return proposals;
-  }
 
   async onModuleInit() {
     this.initMonitor();
@@ -99,174 +47,61 @@ export class MonitoringService implements OnModuleInit, OnModuleDestroy {
       monitorKeypair: this.dialectConnection.getKeypair(),
       dialectProgram: this.dialectConnection.getProgram(),
     })
-      .defineDataSource<RealmData>()
+      .defineDataSource<JupiterArbitrageTrades>()
       .poll(
-        async (subscribers) => this.getRealmsData(subscribers),
-        Duration.fromObject({ seconds: 60 }),
+        async () => this.getJupArbTradeData(),
+        Duration.fromObject({ seconds: 30 }),
       )
-      .transform<ProgramAccount<Proposal>[], ProgramAccount<Proposal>[]>({
-        keys: ['proposals'],
-        pipelines: [Pipelines.added((p1, p2) => p1.pubkey.equals(p2.pubkey))],
+      .transform<ArbTradeData[], ArbTradeData[]>({
+        keys: ['arbTrades'],
+        // TODO compare unique transaction signature, confirm if this is signatures[0]
+        pipelines: [Pipelines.added((p1, p2) => p1.tx.transaction.signatures[0] === p2.tx.transaction.signatures[0])],
       })
       .notify()
-      .dialectThread(
-        ({ value, context }) => {
-          const realmName: string = context.origin.realm.account.name;
-          const realmId: string = context.origin.realm.pubkey.toBase58();
-          const message: string = this.constructMessage(
-            realmName,
-            realmId,
-            value,
-          );
-          this.logger.log(`Sending dialect message: ${message}`);
+      .custom(
+        (val) => {
+          const tokenData: TokenInfo = val.context.origin.arbTrades[0].tokenData;
+          const message = this.constructMessage(val.context.origin);
+          this.logger.log(`Sending tweet for ${tokenData.symbol} arbitrage trade : ${message}`);
+          this.logger.log(message);
           return {
-            message: message,
+            message,
           };
         },
-        (
-          {
-            context: {
-              origin: { realmMembersSubscribedToNotifications },
-            },
-          },
-          recipient,
-        ) => !!realmMembersSubscribedToNotifications[recipient.toBase58()],
+        this.twitterNotificationSink,
+        {
+          dispatch: 'broadcast'
+        }
       )
-      .custom<TwitterNotification>(({ value, context }) => {
-        const realmName: string = context.origin.realm.account.name;
-        const realmId: string = context.origin.realm.pubkey.toBase58();
-        const message = this.constructMessage(realmName, realmId, value);
-        this.logger.log(`Sending tweet for ${realmName} : ${message}`);
-        return {
-          message,
-        };
-      }, this.notificationSink)
       .and()
-      .dispatch('broadcast')
       .build();
     monitor.start();
   }
 
   private constructMessage(
-    realmName: string,
-    realmId: string,
-    proposalsAdded: ProgramAccount<Proposal>[],
+    jupArbTrades: JupiterArbitrageTrades,
   ): string {
     return [
-      ...proposalsAdded.map(
+      ...jupArbTrades.arbTrades.map(
         (it) => {
-          let walletAddress = this.tokenOwnerRecordToGoverningTokenOwner[
-            it.account.tokenOwnerRecord.toBase58()
-          ];
-
-          if (walletAddress) {
-            walletAddress = `${walletAddress.substring(0, 5)}...${walletAddress.substring(walletAddress.length - 5)}`;
-          }
-
-          return `📜 New proposal for ${realmName}: https://realms.today/dao/${realmId}/proposal/${it.pubkey.toBase58()}
-${it.account.name}${
-  walletAddress
-    ? ` added by ${
-        walletAddress
-      }`
-    : ''
-}`;
+          // TODO add useful details about arb trade opportunity
+          return `📈 📉 New arbitrage trade found on Jupiter for ${it.tokenData.symbol}`;
         },
       ),
     ].join('\n');
   }
 
-  private async getRealmsData(
-    subscribers: ResourceId[],
-  ): Promise<SourceData<RealmData>[]> {
+  private async getJupArbTradeData(): Promise<SourceData<JupiterArbitrageTrades>[]> {
     this.logger.log(
-      `Getting realms data for ${subscribers.length} subscribers`,
+      `Getting Jupiter arbitrage trade data.`,
     );
-    const realms = await getRealms(connection, mainnetPK);
-    let realmsPromises = realms.map(async (realm) => {
-      return {
-        realm: realm,
-        proposals: await MonitoringService.getProposals(realm),
-        tokenOwnerRecords: await getAllTokenOwnerRecords(
-          connection,
-          mainnetPK,
-          realm.pubkey,
-        ),
-      };
-    });
-
-    if (process.env.TEST_MODE) {
-      realmsPromises = realmsPromises.slice(0, 20);
-    }
-
-    const subscribersSet = Object.fromEntries(
-      subscribers.map((it) => [it.toBase58(), it]),
-    );
-
-    this.logger.log(
-      `Getting all realms data for ${realmsPromises.length} realms`,
-    );
-    const realmsData = await Promise.all(realmsPromises);
-    this.logger.log(
-      `Completed getting all realms data for ${realmsData.length} realms`,
-    );
-
-    const allProposals: ProgramAccount<Proposal>[] = realmsData
-      .map((it) => {
-        return it.proposals;
-      })
-      .flat();
-
-    this.logger.log(
-      `Getting all proposal owners for ${allProposals.length} proposals`,
-    );
-
-    const proposalsWithOwnerAddressPromises = allProposals.map(
-      async (proposal) => {
-        return {
-          ...proposal,
-          tokenOwnerRecord: await getTokenOwnerRecord(
-            connection,
-            proposal.account.tokenOwnerRecord,
-          ),
-        };
+    const arbTrades: ArbTradeData[] =  await findJupArbTrades();
+    const sourceData: SourceData<JupiterArbitrageTrades> = {
+      groupingKey: 'JupiterArbitrageTrades',
+      data: {
+        arbTrades: arbTrades,
       },
-    );
-
-    const proposalsWithOwnerAddress = await Promise.all(
-      proposalsWithOwnerAddressPromises,
-    );
-
-    this.tokenOwnerRecordToGoverningTokenOwner = Object.fromEntries(
-      proposalsWithOwnerAddress.map((it) => [
-        it.account.tokenOwnerRecord.toBase58(),
-        it.tokenOwnerRecord.account.governingTokenOwner.toBase58(),
-      ]),
-    );
-
-    this.logger.log(
-      `Completed getting all proposal owners for ${allProposals.length} proposals`,
-    );
-
-    return realmsData.map((it) => {
-      const realmMembersSubscribedToNotifications: Record<string, PublicKey> =
-        process.env.TEST_MODE
-          ? Object.fromEntries(subscribers.map((it) => [it.toBase58(), it]))
-          : Object.fromEntries(
-              it.tokenOwnerRecords
-                .map((it) => it.account.governingTokenOwner)
-                .filter((it) => subscribersSet[it.toBase58()])
-                .map((it) => [it.toBase58(), it]),
-            );
-      const sourceData: SourceData<RealmData> = {
-        resourceId: it.realm.pubkey,
-        data: {
-          realm: it.realm,
-          proposals: it.proposals,
-          realmMembersSubscribedToNotifications,
-        },
-      };
-      return sourceData;
-    });
+    };
+    return [sourceData];
   }
 }
